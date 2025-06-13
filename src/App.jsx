61657ab465028aade1eeab60754b297388f469f3
Huzaifa-sheikh2 <huzaifa818481@gmail.com>
2025-06-14 03:27:12 +0500

    create side bar

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,28 @@ import "./App.css";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import Sidebar from "./Components/Sidebar";
+import Createpost from "./Components/Createpost";
+import PostList from "./Components/PostList";
+import { useState } from "react";
 function App() {
+
+  const [selectedTab,setSelectedTab] = useState ("Home")
 
 
   return (
     <>
-    <div className="App-container">
-      
-    <Sidebar></Sidebar>
-    <div className="content">
+      <div className="App-container">
 
-    <Header></Header>
-      <Footer></Footer>
-    </div>
-    </div>
+        <Sidebar selectedTab={selectedTab}></Sidebar>
+        <div className="content">
+
+          <Header></Header>
+          {selectedTab ==="Home"? <PostList></PostList>:<Createpost></Createpost>}
+          
+          
+          <Footer></Footer>
+        </div>
+      </div>
     </>
   )
 }
